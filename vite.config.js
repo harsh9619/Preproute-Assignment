3a1bdf7e62ca24360aa2ca38892c5eb66a89ccf6
Harsh Shukla <38883563+harsh9619@users.noreply.github.com>
2026-07-28 18:35:31 +0530

    added gh-pages

--- vite.config.js
+++ vite.config.js
@@ -10,7 +10,7 @@ export default defineConfig(({ mode }) => {
       port: 5173,
       proxy: {
         '/api': {
-          target: env.VITE_API_TARGET_URL,
+          target: env.VITE_API_TARGET_URL || 'https://admin-moderator-backend-staging.up.railway.app',
           changeOrigin: true,
         }
       }
