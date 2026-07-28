94e0658b0ff9d3c48c87efb7cb851b9b650b30b5
Harsh Shukla <38883563+harsh9619@users.noreply.github.com>
2026-07-28 20:00:07 +0530

    url update

--- src/services/axios.ts
+++ src/services/axios.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // Create configured Axios instance
 const axiosInstance = axios.create({
-  baseURL: 'https://admin-moderator-backend-staging.up.railway.app',
+  baseURL: import.meta.env.VITE_API_URL,
   headers: {
     'Content-Type': 'application/json',
   },
