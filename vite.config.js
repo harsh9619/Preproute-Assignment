3eabec97600e43ce1004cd18610463dc2f3bdbf7
Harsh Shukla <38883563+harsh9619@users.noreply.github.com>
2026-08-21 13:03:50 +0530

    updat

--- vite.config.js
+++ vite.config.js
@@ -6,15 +6,16 @@ export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), '');
   return {
     plugins: [react(), tailwindcss()],
+
     server: {
-      port: 5173,
       proxy: {
-        '/api': {
-          target: env.VITE_API_TARGET_URL,
+        "/api": {
+          target: "https://admin-moderator-backend-staging.up.railway.app",
           changeOrigin: true,
-        }
-      }
-    }
+        },
+      },
+    },
+
   };
 });
 
